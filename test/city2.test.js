43875e631bbfde444ce14f04b07548fa330a2424
lizhongyuan <fenixlee@163.com>
2018-05-28 15:48:18 +0800

    修改一部分变量名称

--- test/city2.test.js
+++ test/city2.test.js
@@ -14,9 +14,9 @@ let loggerOptions = {
 }
 
 let loggerOptions2 = {
-    filename:"what",
+    filename:"lzy-service",
     dir:'/data/log2',
-    app:'what',
+    app:'lzy-service',
     level: 'error'
     //console:true,
 }
@@ -28,23 +28,35 @@ app.use((ctx, next) => {
     if (ctx.url == '/') {
         ctx.cityLogger.setBusinessType("lzyTest").info('city service');
         ctx.cityLogger.info('city service 2');
+
+        // test error
         //ctx.lzyTest.setBusinessType("lzyTest2").error('city service 2');
         //ctx.lzyTest.error('city service 3');
+
+        // test info
         ctx.lzyTest.setBusinessType("lzyTest2").info('city service 2');
         ctx.lzyTest.info('city service 3');
-        
+
         ctx.body = 'city service!';
     }
-    
-    /*
+
     if (ctx.url == '/ping') {
+
+        /*
         ctx.logger.debug('测试debug', {hello: 'city service'});
         ctx.logger.info('测试info', {userId: 21343, info: 'city service'}, {userId: 21343, info: 'city service'});
         ctx.logger.warn('测试warn', {userId: 21343, info: 'city service'}, {userId: 21343, info: 'city service'});
         ctx.logger.error('测试error', new Error('city service'));
+        */
+
+        ctx.cityLogger.debug('测试debug', {hello: 'city service'});
+        ctx.cityLogger.info('测试info', {userId: 21343, info: 'city service'}, {userId: 21343, info: 'city service'});
+        ctx.cityLogger.warn('测试warn', {userId: 21343, info: 'city service'}, {userId: 21343, info: 'city service'});
+        ctx.cityLogger.error('测试error', new Error('city service'));
+
         ctx.body = 'City Service!';
     }
-    */
+
 });
 
 var server = app.listen(port, function () {
