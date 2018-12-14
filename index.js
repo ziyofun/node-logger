c439492f1cb8f3f430793af36c5d19fb4fac3ec3
sleelily <sleelily@live.com>
2018-12-14 21:14:27 +0800

    修改日志打印逻辑，使用ThreadLocal获取traceId

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 "use strict";
 
-const { BaseLogger } = require("./lib/logger");
-const Middleware = require("./lib/middleware/middleware");
-const { globalWinstonInstance } = require('./lib/service/winston');
-
-module.exports = {
-    BaseLogger,
-    globalWinstonInstance,
-    Middleware,
-};
+module.exports = require("./lib/logger");
